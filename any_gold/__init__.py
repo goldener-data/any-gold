9c0b8e9bfd17461ae3b8c8c4a8484c441c86e3ea
Yann-CV <ychene@scortex.io>
2025-05-26 22:42:04 +0200

    Add it in readme and __init__.py

--- any_gold/__init__.py
+++ any_gold/__init__.py
@@ -1,3 +1,11 @@
 from .image.plantseg import PlantSeg
+from .image.deepglobe import DeepGlobeRoadExtraction
+from .image.kpi import KPITask1PatchLevel
+from .image.mvtec_ad import MVTecADDataset
 
-__all__ = ("PlantSeg",)
+__all__ = (
+    "PlantSeg",
+    "DeepGlobeRoadExtraction",
+    "KPITask1PatchLevel",
+    "MVTecADDataset",
+)
