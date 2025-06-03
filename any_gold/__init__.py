5d132b6acc0b2b73747a2e753247438ed1be2c4a
Yann-CV <ychene@scortex.io>
2025-06-03 20:42:13 +0200

    add AnyVisionSegmentationDataset to __init__.py__

--- any_gold/__init__.py
+++ any_gold/__init__.py
@@ -1,10 +1,11 @@
-from .utils.dataset import AnyRawDataset
+from .utils.dataset import AnyRawDataset, AnyVisionSegmentationDataset
 from .image.plantseg import PlantSeg
 from .image.deepglobe import DeepGlobeRoadExtraction
 from .image.kpi import KPITask1PatchLevel
 from .image.mvtec_ad import MVTecADDataset
 
 __all__ = (
+    "AnyVisionSegmentationDataset",
     "AnyRawDataset",
     "PlantSeg",
     "DeepGlobeRoadExtraction",
